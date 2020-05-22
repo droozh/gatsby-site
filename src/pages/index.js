a6869e1b833734c3c04a1ab26300a77ae8d9e186
StarWeb <droozh@mail.ru>
2020-05-22 23:47:17 +0300

    Link removed from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
 
@@ -8,7 +7,7 @@ const indexPage = () => (
         <Head title="Home" />
         <h1>Hello!</h1>
         <h3>Welcome to my site</h3>
-        <p>Need a developer?  <Link to="/contact">Contact me</Link></p>
+        <p>Need a developer?  Contact me</p>
     </Layout>
 )
 
